7c7cde83f0470594916904863804470b096c942e
Ron <ron@rapitec.co.il>
2020-08-11 15:33:08 +0300

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!</p>
+        <Person />
       </div>
+        // <p>This is really working!</p>         NOT ALLOWED
     );
     // return React.createElement('div', { className: 'App' }, '', React.createElement('h1', null, 'Does this work now?'));
   }
