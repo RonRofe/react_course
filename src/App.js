fb4671835aaacdb557a2bbebdc10233499ea6745
Ron <ron@rapitec.co.il>
2020-08-12 08:15:54 +0300

    two way binding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
     });
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 37 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
   render () {
     return (
       <div className="App">
@@ -34,7 +44,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Shimi')}>My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, 'Shimi')}
+          changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
