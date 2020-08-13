6a69b882499960106f74926b4abd4bf6864267c2
Ron <ron@rapitec.co.il>
2020-08-13 07:58:37 +0300

    implement css modules on Person component

--- basics/src/Person/Person.js
+++ basics/src/Person/Person.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #EEE;
-    box-shadow: 0 2px 3px #CCC;
-    padding: 16px;
-    align-items: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
     return (
-        // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>Im a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed}></input>
-        </StyledDiv>
+        </div>
     );
 };
 
