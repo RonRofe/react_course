6fa5ba2f98aeaa5dd0950f576549cf5a75d20f33
Ron <ron@rapitec.co.il>
2020-08-14 17:07:01 +0300

    alternative way to use context and use it outside of the rendering

--- basics/src/components/Persons/Person/Person.js
+++ basics/src/components/Persons/Person/Person.js
@@ -12,9 +12,13 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         // this.inputElement.focus();
         this.inputElementRef.current.focus();
+
+        console.log(this.context.authenticated);
     }
 
     render() {
@@ -23,9 +27,7 @@ class Person extends Component {
         return (
             // <div className={classes.Person}>
             <Fragment>
-                <AuthContext.Consumer>
-                    {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-                </AuthContext.Consumer>
+                { this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
                 <p onClick={this.props.click}>Im a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input
