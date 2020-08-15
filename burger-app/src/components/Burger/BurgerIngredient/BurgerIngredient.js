571221187e4837f8a01ff9ac74f36961110c3d1e
Ron <ron@rapitec.co.il>
2020-08-15 19:10:15 +0300

    starting the burger component

--- burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './burgerIngredient.module.css';
+import classes from './BurgerIngredient.module.css';
 
 class BurgerIngredient extends Component {
     render() {
@@ -25,16 +25,16 @@ class BurgerIngredient extends Component {
                 ingredient = <div className={classes.Meat}></div>;
                 break;
             case 'cheese':
-                ingredient = <div className={classes.Chease}></div>;
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
             case 'salad':
                 ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
-    
-            return ingredient;
         }
+
+        return ingredient;
     }
 };
 
