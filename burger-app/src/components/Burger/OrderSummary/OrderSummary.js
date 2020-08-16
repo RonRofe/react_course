7fc4fde80db426a21da44e39fe9a45e7f202971f
Ron <ron@rapitec.co.il>
2020-08-16 11:43:14 +0300

    performance improvement

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliry';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const $ingredientSummary = Object.keys(props.ingredients).map((key) => {
-        return (
-            <li key={key}>
-                <span style={{ textTransform: 'capitalize' }}>{key}</span>: {props.ingredients[key]}
-            </li>
-        );
-    });
+class OrderSummary extends Component {
+    render() {
+        const $ingredientSummary = Object.keys(this.props.ingredients).map((key) => {
+            return (
+                <li key={key}>
+                    <span style={{ textTransform: 'capitalize' }}>{key}</span>: {this.props.ingredients[key]}
+                </li>
+            );
+        });
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>{$ingredientSummary}</ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button click={props.purcCancel} btnType="Danger">CANCEL</Button>
-            <Button click={props.purcContinue} btnType="Success">CONTINUE</Button>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>{$ingredientSummary}</ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button click={this.props.purcCancel} btnType="Danger">CANCEL</Button>
+                <Button click={this.props.purcContinue} btnType="Success">CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- burger-app/src/components/UI/Modal/Modal.js
+++ burger-app/src/components/UI/Modal/Modal.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliry';
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
-const modal = (props) => {
-    const style = {
-        transform: props.show ? 'TranslateY(0)' : 'TranslateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-    };
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    
+    render() {
+        const style = {
+            transform: this.props.show ? 'TranslateY(0)' : 'TranslateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+        };
 
-    return (
-        <Aux>
-            <Backdrop show={props.show} click={props.close}></Backdrop>
-            <div
-                className={classes.Modal}
-                style={style}>{props.children}</div>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} click={this.props.close}></Backdrop>
+                <div
+                    className={classes.Modal}
+                    style={style}>{this.props.children}</div>
+            </Aux>
+        );
+    }
 };
 
-export default modal;
+export default Modal;
