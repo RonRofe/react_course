7bc1a5cd714fe1f41a07c28f90ec0394f2921bea
Ron <ron@rapitec.co.il>
2020-08-16 09:30:08 +0300

    add a custom button component

---
+++ burger-app/src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const button = (props) => {
+    return (
+        <button
+            className={[classes.button, classes[props.btnType]].join(' ')}
+            onClick={props.click}>{props.children}</button>
+    );
+};
+
+export default button;
