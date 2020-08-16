a3daef402b1fa35c5685e92e99e204a945c9efee
Ron <ron@rapitec.co.il>
2020-08-16 14:52:23 +0300

    http interceptor

--- http/src/index.js
+++ http/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axious from 'axios';
+
+axious.interceptors.request.use((request) =>{
+    console.log(request);
+    return request;
+});
+
+axious.interceptors.response.use((response) => {
+    console.log(response);
+    return response;
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
