48dfb9f9c62419207dd7286ace3048e08e192a13
Ron <ron@rapitec.co.il>
2020-08-17 09:29:42 +0300

    preparing the project for routing

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -1,54 +1,12 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
+import Posts from './Posts/Posts';
+
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-    };
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(({ data }) => {
-                const posts = data.slice(0, 4);
-                const updatedPosts = posts.map((post) => {
-                    return {
-                        ...post,
-                        author: 'Max',
-                    };
-                });
-
-                this.setState({ posts: updatedPosts });
-            })
-            .catch((error) => this.setState({ error: true }));
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    };
-
     render() {
-        let $posts = (
-            <p style={{ textAlign: 'center' }}>Something went wrong!</p>
-        );
-
-        if (!this.state.error) {
-            $posts = this.state.posts.map((post => {
-                return (
-                    <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        click={() => this.postSelectedHandler(post.id)} />
-                );
-            }));
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -59,9 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {$posts}
-                </section>
+                <Posts></Posts>
             </div>
         );
     }
