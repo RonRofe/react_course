20cfccdf851da5523ef0c9b59c33aedfb0b74415
Ron <ron@rapitec.co.il>
2020-08-17 13:07:50 +0300

    redirect programatically

--- http/src/containers/Blog/FullPost/FullPost.js
+++ http/src/containers/Blog/FullPost/FullPost.js
@@ -20,7 +20,7 @@ class FullPost extends Component {
         if (!!this.props.match.params.id) {
             if (
                 !this.state.loadedPost ||
-                (this.state.loadedPost && this.state.loadedPost.id != this.props.match.params.id)
+                (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)
             ) {
                 axious.get('/posts/' + this.props.match.params.id)
                     .then(({ data }) => this.setState({ loadedPost: data }));
--- http/src/containers/Blog/NewPost/NewPost.js
+++ http/src/containers/Blog/NewPost/NewPost.js
@@ -26,7 +26,8 @@ class NewPost extends Component {
         axios.post('https://jsonplaceholder.typicode.com/posts', data)
             .then((response) => {
                 console.log(response);
-                this.setState({ submitted: true });
+                this.props.history.replace('/posts');
+                // this.setState({ submitted: true });
             });
     };
 
