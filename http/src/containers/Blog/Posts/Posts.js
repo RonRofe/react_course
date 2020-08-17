b72ac92bd79966231af3a5983a69fad98589e2e5
Ron <ron@rapitec.co.il>
2020-08-17 09:42:24 +0300

    using routing related props

--- http/src/containers/Blog/Posts/Posts.js
+++ http/src/containers/Blog/Posts/Posts.js
@@ -13,6 +13,7 @@ class Posts extends Component {
     };
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('/posts')
             .then(({ data }) => {
                 const posts = data.slice(0, 4);
